9d6859397c43b3572a18d1c6ccea0f94b3e3fedf
SathishkumarG <be.sathish@gmail.com>
2015-10-02 15:41:03 +0530

    Enabled Paging to limit the items and sorted based on published date

--- src/main/java/com/sathish/jba/repository/ItemRepository.java
+++ src/main/java/com/sathish/jba/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package com.sathish.jba.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sathish.jba.entity.Blog;
@@ -9,6 +10,6 @@ import com.sathish.jba.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
-	List<Item> findByBlog(Blog blog);
+	List<Item> findByBlog(Blog blog, Pageable pageable);
 
 }
